19e3cfc36b03f47d3fe1048df33538133acdb64e
RCaldas Web Code <dev@rcaldas.com>
2021-11-07 18:58:13 +0000

    Save Firefox Profile

--- app/driver.py
+++ app/driver.py
@@ -1,19 +1,72 @@
-from selenium.webdriver import Firefox
+from selenium.webdriver import Firefox, FirefoxProfile
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
-import os
+from datetime import datetime
+# import os
+import random, psutil, shutil, os, time
 
 
+profile_path = '/profile'
+
+profile = FirefoxProfile(profile_path)
 options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-driver = Firefox(options=options)
+options.profile = profile
 
+distinguishkey = "-persistentprofileworkaround"+str(random.randint(111111,999999))
+options.add_argument(distinguishkey)
+
+options.add_argument('--headless')
+# options.add_argument('--no-sandbox')
+# options.add_argument('--disable-dev-shm-usage')
+driver = Firefox(options=options)
 
 driver.get("https://web.whatsapp.com")
 
-driver.save_screenshot('screen.png')
+time.sleep(20)
+
+#! Detect qrcode and ask for scan
+driver.save_screenshot(f"/app/{datetime.now().strftime('%Y-%m-%d_%H-%M')}_screen.png")
+
+# time.sleep(120)
+
+for pid in psutil.pids():
+	try:
+		cmdline = open("/proc/"+str(pid)+"/cmdline", "r").read()
+		if distinguishkey in cmdline:
+		  profile = cmdline.split('-profile')[1].split(' ')[0].replace('\x00', '')
+		  break
+	except:
+		pass
+
+psutil.Process(pid).kill() # kill firefox (nicely) and unlock profile lock
+if os.path.isdir(profile_path):
+	shutil.rmtree(profile_path, ignore_errors=True)
+shutil.copytree(profile, profile_path, symlinks=True, dirs_exist_ok=True) # copy the new profile to profile_path, don't resolve "lock" symlink
+
+try:
+    driver.quit() # will throw an error because we killed firefox
+except Exception as e:
+    print(e)
+    pass
+
+# cleanup
+if os.path.isdir(profile):
+	shutil.rmtree(profile)
+if os.path.isdir(driver.profile.tempfolder):
+	shutil.rmtree(driver.profile.tempfolder)
+
+
+
+
+# profiletmp = driver.firefox_profile.path
+
+# if os.system("cp -R " + profiletmp + "/* /profile/" ):
+#     print("files should be copied :/")
+
+
+# driver.quit()
+
+
+# options.headless = True
 
 
 # name = "Whatsapp Bot"
@@ -48,3 +101,5 @@ driver.save_screenshot('screen.png')
 #     elif lastMsg=='/quit':
 #         sendText('Quitting...', driver)
 #         executing=False
+
+
